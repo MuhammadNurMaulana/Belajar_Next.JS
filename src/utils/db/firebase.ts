703bf07104fc7748d387fd9c4b7717060aa5e02f
“MuhammadNurMaulana” <muhammadnrmaulana@gmail.com>
2023-10-15 10:18:52 +0700

    tes

--- src/utils/db/firebase.ts
+++ src/utils/db/firebase.ts
@@ -17,22 +17,3 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export default app;
-
-// // Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
-
-// // Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: "AIzaSyB9h-wcLpgx1ojOSgI5hoOfnf0KhCCiq9E",
-//   authDomain: "maulana-e9616.firebaseapp.com",
-//   projectId: "maulana-e9616",
-//   storageBucket: "maulana-e9616.appspot.com",
-//   messagingSenderId: "14068393493",
-//   appId: "1:14068393493:web:1cf089f2eca3de32a6901c",
-// };
-
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// export default app;
